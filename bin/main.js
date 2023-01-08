748456464ff7eb5b226cdcbc7c39245a9272d6fe
faraz faraji <faraz.faraji@gmail.com>
2023-01-08 21:12:58 +0300

    fix bin issue

--- bin/main.js
+++ bin/main.js
@@ -1,15 +1,15 @@
+#! /usr/bin/env node
+const fs = require("fs")
 
-// const fs = require("fs")
+function run(){
+  if(fs.existsSync("src"))
+    throw new Error("src folder is already exist. Please remove it before start");
 
-// function run(){
-//   if(fs.existsSync("src"))
-//     throw new Error("src folder is already exist. Please remove it before start");
+  fs.mkdirSync("src");
+  fs.copyFileSync("./template/main.js","src/main.js")
 
-//   fs.mkdirSync("src");
-//   fs.copyFileSync("./template/main.js","src/main.js")
+}
 
-// }
-
-// run()
+run()
 
 console.log("salam")
