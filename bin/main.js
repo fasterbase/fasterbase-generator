8c0a04dfb001618d579bc96db01a4117277a18a2
faraz faraji <faraz.faraji@gmail.com>
2023-01-09 18:24:21 +0300

    fix the run command

--- bin/main.js
+++ bin/main.js
@@ -2,12 +2,21 @@
 const fs = require("fs")
 
 function run(){
+  const params = process.argv;
+  
+  if(!params[2])
+  throw new Error("API did not provided");
+
   if(fs.existsSync("src"))
     throw new Error("src folder is already exist. Please remove it before start");
 
   fs.mkdirSync("src");
-  fs.copyFileSync("./template/main.js","src/main.js")
-
+  fs.copyFileSync(__dirname +"/template/main.js","src/main.js")
+  fs.copyFileSync(__dirname +"/template/meta.json","src/meta.json")
+  fs.copyFileSync(__dirname +"/template/publish.sh","src/publish.sh")
+  const sh = fs.readFileSync("src/publish.sh","utf8");
+  
+  fs.writeFileSync("src/publish.sh",sh.replace("TOKEN",params[2]))
 }
 
 run()
